60fb180bd01a2c443f0d326ff9a1c47b042f9201
shankar87dayal <shankar.87.dayal@gmail.com>
2023-01-02 06:12:27 +0530

    create Order and Payment JPA Entities

---
+++ src/main/java/com/javawithraushan/springboot/entity/Payment.java
@@ -0,0 +1,28 @@
+package com.javawithraushan.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String type;
+    private String cardName;
+    private String cardNumber;
+    private int expiryYear;
+    private int expiryMonth;
+    private int cvc;
+    private long orderId;
+
+}
